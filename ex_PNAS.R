af18a9ac8a23397f28ddf53da95ede1356cc9a73
sbacelar <sergio.bacelar@gmail.com>
2017-01-03 14:33:56 +0000

    eigenvector not equal to 1

--- ex_PNAS.R
+++ ex_PNAS.R
@@ -1,11 +1,27 @@
-# Atlas da complexidade
+# Atlas of Economic Complexity
 #
 X<-matrix(c(1,1,1,1,0,1,0,0,0,0,1,1,0,0,0,1), + 4,4,byrow=T)
 X
-kc <- rowSums(X)
-kp <- colSums(X)
-kc
-kp
-1/kc
-1/kc * (X %*% kp)
-1/kp * (t(X) %*% kc)
+kc_1 <- rowSums(X) # Diversity
+kp_1 <- colSums(X) # Ubiquity
+
+kc_2 <- 1/kc_1 * (X %*% kp_1)
+kp_2 <- 1/kp_1 * (t(X) %*% kc_1)
+
+kc_3 <- 1/kc_1 * (X %*% kp_2) # right
+kc_3_a <- 1/kc_1 * (X %*% (1/kp_1 * (t(X) %*% kc_1))) # right
+
+kc_3_b <- diag(1/kc_1) %*% X %*% diag(1/kp_1) %*% t(X) %*% kc_1 # right
+kc_3; kc_3_a; kc_3_b # Same result
+
+Mc <- diag(1/kc_1) %*% X %*% diag(1/kp_1) %*% t(X)
+e <- eigen (Mc)
+# Problem: the first eigen vector is 0.5 and not 1
+ECI <- e$vectors[,2]
+
+
+
+# PRODUCT
+kp_3 <- 1/kp_1 * (t(X) %*% kc_2)
+
+
